717093cc781afa1768c8aa50041342e6a3a6f436
Xtalism <manuel_lego1234@hotmail.com>
2025-09-23 18:52:44 -0600

    YASMIN state machine

--- src/state_machine/src/yasmin_dji_tello.cpp
+++ src/state_machine/src/yasmin_dji_tello.cpp
@@ -20,6 +20,26 @@ using std::placeholders::_1;
 using std::placeholders::_2;
 using namespace yasmin;
 
+
+class PublishPingState
+    : public yasmin_ros::PublisherState<std_msgs::msg::Empty> {
+
+public:
+  PublishPingState()
+      : yasmin_ros::PublisherState<std_msgs::msg::Empty>(
+            "ping_success", // topic name
+            std::bind(&PublishPingState::create_empty_msg, this,
+                      _1) // create msg handler callback
+        ){};
+
+  std_msgs::msg::Empty
+  create_empty_msg(std::shared_ptr<yasmin::blackboard::Blackboard> blackboard) {
+    YASMIN_LOG_INFO("pinging...");
+    std_msgs::msg::Empty msg;
+    return msg;
+  };
+};
+
 class PublishTakeoffState
     : public yasmin_ros::PublisherState<std_msgs::msg::Empty> {
 
@@ -39,6 +59,41 @@ public:
   };
 };
 
+
+class PublishLandingState
+    : public yasmin_ros::PublisherState<std_msgs::msg::Empty> {
+
+public:
+  PublishLandingState()
+      : yasmin_ros::PublisherState<std_msgs::msg::Empty>(
+            "land", // topic name
+            std::bind(&PublishLandingState::create_empty_msg, this,
+                      _1) // create msg handler callback
+        ){};
+
+  std_msgs::msg::Empty
+  create_empty_msg(std::shared_ptr<yasmin::blackboard::Blackboard> blackboard) {
+    YASMIN_LOG_INFO("Publishing landing message");
+    std_msgs::msg::Empty msg;
+    return msg;
+  };
+};
+
+std::string
+check_connection(std::shared_ptr<yasmin::blackboard::Blackboard> blackboard) {
+
+  // Sleep for 1 second to simulate some processing time
+  rclcpp::sleep_for(std::chrono::seconds(1));
+  YASMIN_LOG_INFO("pinging_connection...");
+
+  return "ping_success";
+  // if (blackboard->get<int>("counter") >= blackboard->get<int>("max_count")) {
+  //   return "taking_off";
+  // } else {
+  //   return "waiting_connection";
+  // }
+}
+
 std::string
 check_takeoff(std::shared_ptr<yasmin::blackboard::Blackboard> blackboard) {
 
@@ -46,12 +101,54 @@ check_takeoff(std::shared_ptr<yasmin::blackboard::Blackboard> blackboard) {
   rclcpp::sleep_for(std::chrono::seconds(1));
   YASMIN_LOG_INFO("Checking takeoff...");
 
-  if (blackboard->get<int>("counter") >= blackboard->get<int>("max_count")) {
-    return "taking_off";
-  } else {
-    return "waiting_connection";
-  }
+  return "taking_off";
+  // if (blackboard->get<int>("counter") >= blackboard->get<int>("max_count")) {
+  //   return "taking_off";
+  // } else {
+  //   return "waiting_connection";
+  // }
 }
+
+std::string
+check_forward(std::shared_ptr<yasmin::blackboard::Blackboard> blackboard) {
+
+  // Sleep for 1 second to simulate some processing time
+  rclcpp::sleep_for(std::chrono::seconds(1));
+  YASMIN_LOG_INFO("Pitching forward...");
+
+  return "pitch_forward";
+}
+
+std::string
+check_landing(std::shared_ptr<yasmin::blackboard::Blackboard> blackboard) {
+
+  // Sleep for 1 second to simulate some processing time
+  rclcpp::sleep_for(std::chrono::seconds(1));
+  YASMIN_LOG_INFO("Checking land...");
+
+  return "land";
+}
+
+std::string
+check_grounded(std::shared_ptr<yasmin::blackboard::Blackboard> blackboard) {
+
+  // Sleep for 1 second to simulate some processing time
+  rclcpp::sleep_for(std::chrono::seconds(1));
+  YASMIN_LOG_INFO("grounded...");
+
+  return "grounded";
+}
+
+std::string
+check_error(std::shared_ptr<yasmin::blackboard::Blackboard> blackboard) {
+
+  // Sleep for 1 second to simulate some processing time
+  rclcpp::sleep_for(std::chrono::seconds(1));
+  YASMIN_LOG_INFO("Checking takeoff...");
+
+  return "error";
+}
+
 
 int main(int argc, char *argv[]) {
 
@@ -61,7 +158,9 @@ int main(int argc, char *argv[]) {
   yasmin_ros::set_ros_loggers();
 
   auto sm = std::make_shared<yasmin::StateMachine>(
-      std::initializer_list<std::string>{yasmin_ros::basic_outcomes::SUCCEED});
+      std::set<std::string>{
+          yasmin_ros::basic_outcomes::SUCCEED,
+      });
 
   rclcpp::on_shutdown([sm]() {
     if (sm->is_running()) {
@@ -69,17 +168,40 @@ int main(int argc, char *argv[]) {
     }
   });
 
-  sm->add_state("PUBLISHING_TAKEOFF", std::make_shared<PublishTakeoffState>(),
+  sm->add_state("PING_CONNECTION", std::make_shared<PublishPingState>(),
                 {
                     {yasmin_ros::basic_outcomes::SUCCEED,
-                     "PUBLISHING_TAKEOFF"}, // Transition back to itself
+                     "CONNECTION_ESTABLISHED"},
                 });
-  sm->add_state("CHECKING_TAKEOFF_STATUS",
+  sm->add_state("CONNECTION_ESTABLISHED", std::make_shared<PublishTakeoffState>(),
+                {
+                    {yasmin_ros::basic_outcomes::SUCCEED,
+                     "VERIFY_SENSORS"},
+                });
+  sm->add_state("VERIFY_SENSORS",
                 std::make_shared<yasmin::CbState>(
                     std::initializer_list<std::string>{"taking_off", "waiting_connection"},
                     check_takeoff),
-                {{"waiting_connection", yasmin_ros::basic_outcomes::SUCCEED},
-                 {"taking_off", "PUBLISHING_TAKEOFF"}});
+              {{"taking_off", "PITCH_FORWARD"},
+               {"waiting_connection", "CONNECTION_ESTABLISHED"}});
+  sm->add_state("PITCH_FORWARD",
+                std::make_shared<yasmin::CbState>(
+                    std::initializer_list<std::string>{"pitch_forward", "waiting_connection"},
+                    check_forward),
+                {{"pitch_forward", "LANDING"},
+                 {"waiting_connection", "CONNECTION_ESTABLISHED"}});
+  sm->add_state("LANDING",
+                std::make_shared<yasmin::CbState>(
+                    std::initializer_list<std::string>{"land", "waiting_connection"},
+                    check_landing),
+              {{"land", "GROUND"},
+               {"waiting_connection", "CONNECTION_ESTABLISHED"}});
+  sm->add_state("GROUND",
+                std::make_shared<yasmin::CbState>(
+                    std::initializer_list<std::string>{"grounded", "waiting_connection"},
+                    check_grounded),
+              {{"grounded", yasmin_ros::basic_outcomes::SUCCEED},
+               {"waiting_connection", "CONNECTION_ESTABLISHED"}});
 
   yasmin_viewer::YasminViewerPub yasmin_pub("YASMIN_DJI_TELLO", sm);
 
--- src/tello/tello/node.py
+++ src/tello/tello/node.py
@@ -1,62 +1,58 @@
 #!/usr/bin/env python3
 
-import pprint
 import math
-import rclpy
 import threading
-import numpy
 import time
-import av
-import tf2_ros
-import cv2
-import time
-import yaml
 
-from djitellopy import Tello
-
-from rclpy.node import Node
-from tello_msg.msg import TelloStatus, TelloID, TelloWifiConfig
-from std_msgs.msg import Empty, UInt8, UInt8, Bool, String
-from sensor_msgs.msg import Image, Imu, BatteryState, Temperature, CameraInfo
-from geometry_msgs.msg import Twist, TransformStamped
-from nav_msgs.msg import Odometry
-from cv_bridge import CvBridge
 import ament_index_python
+import numpy
+import rclpy
+import tf2_ros
+import yaml
+from cv_bridge import CvBridge
+from djitellopy import Tello
+from geometry_msgs.msg import TransformStamped, Twist
+from nav_msgs.msg import Odometry
+from sensor_msgs.msg import BatteryState, CameraInfo, Image, Imu, Temperature
+from std_msgs.msg import Empty, String
+
+from tello_msg.msg import TelloID, TelloStatus, TelloWifiConfig
+
 
 # Tello ROS node class, inherits from the Tello controller object.
 #
 # Can be configured to be used by multiple drones, publishes, all data collected from the drone and provides control using ROS messages.
-class TelloNode():
+class TelloNode:
     def __init__(self, node):
         # ROS node
         self.node = node
 
         # Declare parameters
-        self.node.declare_parameter('connect_timeout', 10.0)
-        self.node.declare_parameter('tello_ip', '192.168.10.1')
-        self.node.declare_parameter('tf_base', 'map')
-        self.node.declare_parameter('tf_drone', 'drone')
-        self.node.declare_parameter('tf_pub', False)
-        self.node.declare_parameter('camera_info_file', '')
+        self.node.declare_parameter("connect_timeout", 10.0)
+        self.node.declare_parameter("tello_ip", "192.168.10.1")
+        self.node.declare_parameter("tf_base", "map")
+        self.node.declare_parameter("tf_drone", "drone")
+        self.node.declare_parameter("tf_pub", False)
+        self.node.declare_parameter("camera_info_file", "")
 
         # Get parameters
-        self.connect_timeout = float(self.node.get_parameter('connect_timeout').value)
-        self.tello_ip = str(self.node.get_parameter('tello_ip').value)
-        self.tf_base = str(self.node.get_parameter('tf_base').value)
-        self.tf_drone = str(self.node.get_parameter('tf_drone').value)
-        self.tf_pub = bool(self.node.get_parameter('tf_pub').value)
-        self.camera_info_file = str(self.node.get_parameter('camera_info_file').value)
+        self.connect_timeout = float(self.node.get_parameter("connect_timeout").value)
+        self.tello_ip = str(self.node.get_parameter("tello_ip").value)
+        self.tf_base = str(self.node.get_parameter("tf_base").value)
+        self.tf_drone = str(self.node.get_parameter("tf_drone").value)
+        self.tf_pub = bool(self.node.get_parameter("tf_pub").value)
+        self.camera_info_file = str(self.node.get_parameter("camera_info_file").value)
 
         # Camera information loaded from calibration yaml
         self.camera_info = None
-        
+
         # Check if camera info file was received as argument
         if len(self.camera_info_file) == 0:
-            share_directory = ament_index_python.get_package_share_directory('tello')
-            self.camera_info_file = share_directory + '/ost.yaml'
+            share_directory = ament_index_python.get_package_share_directory("tello")
+            self.camera_info_file = share_directory + "/ost.yaml"
 
         # Read camera info from YAML file
-        with open(self.camera_info_file, 'r') as file:
+        with open(self.camera_info_file, "r") as file:
             self.camera_info = yaml.load(file, Loader=yaml.FullLoader)
             # self.node.get_logger().info('Tello: Camera information YAML' + self.camera_info.__str__())
 
@@ -65,12 +61,12 @@ class TelloNode():
         Tello.RESPONSE_TIMEOUT = int(self.connect_timeout)
 
         # Connect to drone
-        self.node.get_logger().info('Tello: Connecting to drone')
+        self.node.get_logger().info("Tello: Connecting to drone")
 
         self.tello = Tello()
         self.tello.connect()
 
-        self.node.get_logger().info('Tello: Connected to drone')
+        self.node.get_logger().info("Tello: Connected to drone")
 
         # Publishers and subscribers
         self.setup_publishers()
@@ -81,43 +77,53 @@ class TelloNode():
         self.start_tello_status()
         self.start_tello_odom()
 
-        self.node.get_logger().info('Tello: Driver node ready')
+        self.node.get_logger().info("Tello: Driver node ready")
 
     # Setup ROS publishers of the node.
     def setup_publishers(self):
-        self.pub_image_raw = self.node.create_publisher(Image, 'image_raw', 1)
-        self.pub_camera_info = self.node.create_publisher(CameraInfo, 'camera_info', 1)
-        self.pub_status = self.node.create_publisher(TelloStatus, 'status', 1)
-        self.pub_id = self.node.create_publisher(TelloID, 'id', 1)
-        self.pub_imu = self.node.create_publisher(Imu, 'imu', 1)
-        self.pub_battery = self.node.create_publisher(BatteryState, 'battery', 1)
-        self.pub_temperature = self.node.create_publisher(Temperature, 'temperature', 1)
-        self.pub_odom = self.node.create_publisher(Odometry, 'odom', 1)
+        self.pub_image_raw = self.node.create_publisher(Image, "image_raw", 1)
+        self.pub_camera_info = self.node.create_publisher(CameraInfo, "camera_info", 1)
+        self.pub_status = self.node.create_publisher(TelloStatus, "status", 1)
+        self.pub_id = self.node.create_publisher(TelloID, "id", 1)
+        self.pub_imu = self.node.create_publisher(Imu, "imu", 1)
+        self.pub_battery = self.node.create_publisher(BatteryState, "battery", 1)
+        self.pub_temperature = self.node.create_publisher(Temperature, "temperature", 1)
+        self.pub_odom = self.node.create_publisher(Odometry, "odom", 1)
 
         # TF broadcaster
         if self.tf_pub:
             self.tf_broadcaster = tf2_ros.TransformBroadcaster(self.node)
-    
+
     # Setup the topic subscribers of the node.
     def setup_subscribers(self):
-        self.sub_emergency = self.node.create_subscription(Empty, 'emergency', self.cb_emergency, 1)
-        self.sub_takeoff = self.node.create_subscription(Empty, 'takeoff', self.cb_takeoff, 1)
-        self.sub_land = self.node.create_subscription(Empty, 'land', self.cb_land, 1)
-        self.sub_control = self.node.create_subscription(Twist, 'control', self.cb_control, 1)
-        self.sub_flip = self.node.create_subscription(String, 'flip', self.cb_flip, 1)
-        self.sub_wifi_config = self.node.create_subscription(TelloWifiConfig, 'wifi_config', self.cb_wifi_config, 1)
+        self.sub_emergency = self.node.create_subscription(
+            Empty, "emergency", self.cb_emergency, 1
+        )
+        self.sub_takeoff = self.node.create_subscription(
+            Empty, "takeoff", self.cb_takeoff, 1
+        )
+        self.sub_land = self.node.create_subscription(Empty, "land", self.cb_land, 1)
+        self.sub_control = self.node.create_subscription(
+            Twist, "control", self.cb_control, 1
+        )
+        self.sub_flip = self.node.create_subscription(String, "flip", self.cb_flip, 1)
+        self.sub_wifi_config = self.node.create_subscription(
+            TelloWifiConfig, "wifi_config", self.cb_wifi_config, 1
+        )
 
     # Get the orientation of the drone as a quaternion
     def get_orientation_quaternion(self):
         deg_to_rad = math.pi / 180.0
-        return euler_to_quaternion([
-            self.tello.get_yaw() * deg_to_rad,
-            self.tello.get_pitch() * deg_to_rad,
-            self.tello.get_roll() * deg_to_rad
-        ])
+        return euler_to_quaternion(
+            [
+                self.tello.get_yaw() * deg_to_rad,
+                self.tello.get_pitch() * deg_to_rad,
+                self.tello.get_roll() * deg_to_rad,
+            ]
+        )
 
     # Start drone info thread
-    def start_tello_odom(self, rate=1.0/10.0):
+    def start_tello_odom(self, rate=1.0 / 10.0):
         def status_odom():
             while True:
                 # TF
@@ -130,7 +136,7 @@ class TelloNode():
                     t.transform.translation.y = 0.0
                     t.transform.translation.z = (self.tello.get_barometer()) / 100.0
                     self.tf_broadcaster.sendTransform(t)
-                
+
                 # IMU
                 if self.pub_imu.get_subscription_count() > 0:
                     q = self.get_orientation_quaternion()
@@ -158,11 +164,17 @@ class TelloNode():
                     odom_msg.pose.pose.orientation.y = q[1]
                     odom_msg.pose.pose.orientation.z = q[2]
                     odom_msg.pose.pose.orientation.w = q[3]
-                    odom_msg.twist.twist.linear.x = float(self.tello.get_speed_x()) / 100.0
-                    odom_msg.twist.twist.linear.y = float(self.tello.get_speed_y()) / 100.0
-                    odom_msg.twist.twist.linear.z = float(self.tello.get_speed_z()) / 100.0
+                    odom_msg.twist.twist.linear.x = (
+                        float(self.tello.get_speed_x()) / 100.0
+                    )
+                    odom_msg.twist.twist.linear.y = (
+                        float(self.tello.get_speed_y()) / 100.0
+                    )
+                    odom_msg.twist.twist.linear.z = (
+                        float(self.tello.get_speed_z()) / 100.0
+                    )
                     self.pub_odom.publish(odom_msg)
-                
+
                 time.sleep(rate)
 
         thread = threading.Thread(target=status_odom)
@@ -170,7 +182,7 @@ class TelloNode():
         return thread
 
     # Start drone info thread
-    def start_tello_status(self, rate=1.0/2.0):
+    def start_tello_status(self, rate=1.0 / 2.0):
         def status_loop():
             while True:
                 # Battery
@@ -181,8 +193,8 @@ class TelloNode():
                     msg.voltage = 3.8
                     msg.design_capacity = 1.1
                     msg.present = True
-                    msg.power_supply_technology = 2 # POWER_SUPPLY_TECHNOLOGY_LION
-                    msg.power_supply_status = 2 # POWER_SUPPLY_STATUS_DISCHARGING
+                    msg.power_supply_technology = 2  # POWER_SUPPLY_TECHNOLOGY_LION
+                    msg.power_supply_status = 2  # POWER_SUPPLY_STATUS_DISCHARGING
                     self.pub_battery.publish(msg)
 
                 # Temperature
@@ -241,7 +253,7 @@ class TelloNode():
                     msg.R = self.camera_info.rectification_matrix
                     msg.P = self.camera_info.projection_matrix
                     self.pub_camera_info.publish(msg)
-                
+
                 # Sleep
                 time.sleep(rate)
 
@@ -249,9 +261,8 @@ class TelloNode():
         thread.start()
         return thread
 
-
     # Start video capture thread.
-    def start_video_capture(self, rate=1.0/30.0):
+    def start_video_capture(self, rate=1.0 / 30.0):
         # Enable tello stream
         self.tello.streamon()
 
@@ -266,12 +277,11 @@ class TelloNode():
                 frame = frame_read.frame
 
                 # Publish opencv frame using CV bridge
-                msg = self.bridge.cv2_to_imgmsg(numpy.array(frame), 'bgr8')
+                msg = self.bridge.cv2_to_imgmsg(numpy.array(frame), "bgr8")
                 msg.header.frame_id = self.tf_drone
                 self.pub_image_raw.publish(msg)
 
                 time.sleep(rate)
-                
 
         # We need to run the recorder in a seperate thread, otherwise blocking options would prevent frames from getting added to the video
         thread = threading.Thread(target=video_capture_thread)
@@ -302,28 +312,40 @@ class TelloNode():
     #
     # Receives the linear and angular velocities to be applied from -100 to 100.
     def cb_control(self, msg):
-        self.tello.send_rc_control(int(msg.linear.x), int(msg.linear.y), int(msg.linear.z), int(msg.angular.z))
+        self.tello.send_rc_control(
+            int(msg.linear.x), int(msg.linear.y), int(msg.linear.z), int(msg.angular.z)
+        )
 
     # Configure the wifi credential that should be used by the drone.
     #
     # The drone will be restarted after the credentials are changed.
     def cb_wifi_config(self, msg):
         self.tello.set_wifi_credentials(msg.ssid, msg.password)
-    
+
     # Perform a drone flip in a direction specified.
-    # 
+    #
     # Directions can be "r" for right, "l" for left, "f" for forward or "b" for backward.
     def cb_flip(self, msg):
         self.tello.flip(msg.data)
 
+
 # Convert a rotation from euler to quaternion.
 def euler_to_quaternion(r):
     (yaw, pitch, roll) = (r[0], r[1], r[2])
-    qx = math.sin(roll/2) * math.cos(pitch/2) * math.cos(yaw/2) - math.cos(roll/2) * math.sin(pitch/2) * math.sin(yaw/2)
-    qy = math.cos(roll/2) * math.sin(pitch/2) * math.cos(yaw/2) + math.sin(roll/2) * math.cos(pitch/2) * math.sin(yaw/2)
-    qz = math.cos(roll/2) * math.cos(pitch/2) * math.sin(yaw/2) - math.sin(roll/2) * math.sin(pitch/2) * math.cos(yaw/2)
-    qw = math.cos(roll/2) * math.cos(pitch/2) * math.cos(yaw/2) + math.sin(roll/2) * math.sin(pitch/2) * math.sin(yaw/2)
+    qx = math.sin(roll / 2) * math.cos(pitch / 2) * math.cos(yaw / 2) - math.cos(
+        roll / 2
+    ) * math.sin(pitch / 2) * math.sin(yaw / 2)
+    qy = math.cos(roll / 2) * math.sin(pitch / 2) * math.cos(yaw / 2) + math.sin(
+        roll / 2
+    ) * math.cos(pitch / 2) * math.sin(yaw / 2)
+    qz = math.cos(roll / 2) * math.cos(pitch / 2) * math.sin(yaw / 2) - math.sin(
+        roll / 2
+    ) * math.sin(pitch / 2) * math.cos(yaw / 2)
+    qw = math.cos(roll / 2) * math.cos(pitch / 2) * math.cos(yaw / 2) + math.sin(
+        roll / 2
+    ) * math.sin(pitch / 2) * math.sin(yaw / 2)
     return [qx, qy, qz, qw]
+
 
 # Convert rotation from quaternion to euler.
 def quaternion_to_euler(q):
@@ -340,10 +362,11 @@ def quaternion_to_euler(q):
     yaw = math.atan2(t3, t4)
     return [yaw, pitch, roll]
 
+
 def main(args=None):
     rclpy.init(args=args)
 
-    node = rclpy.create_node('tello')
+    node = rclpy.create_node("tello")
     drone = TelloNode(node)
 
     rclpy.spin(node)
@@ -352,5 +375,6 @@ def main(args=None):
     node.destroy_node()
     rclpy.shutdown()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
